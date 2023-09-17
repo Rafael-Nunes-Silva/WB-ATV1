759423d10ec99e59e379d3a34924883e2bda47be
Rafael-Nunes-Silva <rafaelnunesilva20@gmail.com>
2023-09-17 12:37:48 -0300

    Manager de produtos

---
+++ src/Business/ProdutoManager.ts
@@ -0,0 +1,12 @@
+import IOManager from "../IO/IOManager";
+import Produto from "../Models/Produto";
+
+export default class ProdutoManager {
+    public static NovoProduto(): Produto {
+        console.log("Cadastro de produto:")
+        const nome = IOManager.GetString("Nome");
+        const valor = IOManager.GetFloat("Valor");
+
+        return new Produto(nome, valor);
+    }
+}
